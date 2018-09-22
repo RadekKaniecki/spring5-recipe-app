8edd5970fe789742e568b73afac61b1c77b739e5
Radek Kaniecki <radoslawkaniecki@gmail.com>
2018-09-22 15:41:59 +0200

    added functionality to create new recipe, closes #1

--- src/main/java/rkaniecki/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/rkaniecki/spring5recipeapp/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package rkaniecki.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rkaniecki.spring5recipeapp.commands.RecipeCommand;
 import rkaniecki.spring5recipeapp.services.RecipeService;
 
 @Controller
@@ -15,12 +18,24 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
         return "recipe/show";
     }
 
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
 
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipe.getId();
+    }
 }
